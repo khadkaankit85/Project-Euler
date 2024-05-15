bc40897e11864543a160a87c04e85fd2ec55614d
Angkit Khadka <khadkaankit85@gmail.com>
2024-05-15 03:10:57 -0400

    type: solution note: question number two solved

---
+++ Solutions/Problem 2: Even Fibonacci Numbers.js
@@ -0,0 +1,37 @@
+function fiboEvenSum(n) {
+    //lets find fibonacci numbers first
+    let series = []
+    function findFibonacci(n) {
+        if (n <= 0) {
+            return 0
+        }
+        else if (n == 1) {
+            return 1
+        }
+        else {
+            return findFibonacci(n - 1) + findFibonacci(n - 2)
+        }
+    }
+    let i = 2
+
+    while (i <= n) {
+        let myFibonacciNumber = findFibonacci(i)
+        if (myFibonacciNumber > n) {
+            break
+        }
+        console.log("appending ", findFibonacci(i))
+        series.push(myFibonacciNumber)
+        i++
+
+
+
+    }
+    let sum = (series.filter(number => number % 2 == 0)).reduce((p, c) => p + c, 0)
+
+
+    console.log(series)
+    console.log(sum)
+
+    return true;
+}
+fiboEvenSum(1000)
