a1ff5bc76a5b457afdfd845b6ebde93a1fc0967f
Angkit Khadka <khadkaankit85@gmail.com>
2024-05-18 03:24:51 -0400

    Type:Solution-6 Note:Question 6 solved with ease

--- Solutions/Problem 6: Sum square difference.js
+++ Solutions/Problem 6: Sum square difference.js
@@ -1,6 +1,16 @@
 function sumSquareDifference(n) {
+    let myArray = Array.from(Array(n + 1).keys())
+    // let's remove 0, as its not a natural number
+    let realArray = myArray.filter((i) => i != 0)
+    let sumOfSquare = (realArray.map((i) => i * i)).reduce((p, c) => {
+        return (p + c)
+    }, 0)
+    let squareOfSum = (realArray.reduce((p, c) => {
+        return p + c
+    }, 0)) ** 2
 
-    return true;
+
+    return squareOfSum - sumOfSquare;
 }
 
-sumSquareDifference(100);
+sumSquareDifference(100)
