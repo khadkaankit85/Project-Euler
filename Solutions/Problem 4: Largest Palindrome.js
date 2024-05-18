2d07632a14b7e91554f5f293619fd3c4858e5919
Angkit Khadka <khadkaankit85@gmail.com>
2024-05-17 22:05:48 -0400

    Type: Solution Note: Question Number 4 solved, Largest Palindrome

--- Solutions/Problem 4: Largest Palindrome.js
+++ Solutions/Problem 4: Largest Palindrome.js
@@ -1,4 +1,38 @@
 function largestPalindromeProduct(n) {
+    let maxPalindromeNumber = -1
+
+    // lets create a function to check if a number is palindrome or not
+    function isPalindrome(number) {
+
+        // first is converted to string then splitted to make an array then reversed using an array method and joinded later
+        let reversedNumber = number.toString().split("").reverse().join("")
+        if (number.toString() === reversedNumber) {
+            console.log("that was a palindrome")
+            return true
+        }
+        else {
+            console.log("That was not a palindrome")
+            return false
+        }
+    }
+    let largestNumber = Math.pow(10, n) - 1
+    const smallestNumber = Math.pow(10, n - 1)
+    console.log(largestNumber, smallestNumber)
+
+    // let's calculate each number to check if thats a palindrome
+    for (let i = largestNumber; i >= smallestNumber; i--) {
+        for (let j = largestNumber; j >= smallestNumber; j--) {
+            // console.log("Multiplying ", i, " and", j)
+            let product = i * j
+            console.log("checking.................. ", product)
+            if (isPalindrome(product) && maxPalindromeNumber <= product) {
+                console.log(product, " is a palindrom number")
+                maxPalindromeNumber = product
+            }
+        }
+    }
+    console.log("Max palindrom number is ", maxPalindromeNumber)
+
 
     return true;
 }
