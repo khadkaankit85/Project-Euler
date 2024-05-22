9f9ac37790423af2f986d9f7036d9dd6f271659f
Angkit Khadka <khadkaankit85@gmail.com>
2024-05-22 10:01:13 -0400

    Type: solution Note: Solution for problem 14

---
+++ Solutions/Problem 15: Lattice paths.js
@@ -0,0 +1,16 @@
+
+//number of ways to rich n from 2n is coefficient of n or something
+//we use count =(2n!)/(n!*n!) to find the solution so
+
+//lets create a function to find factorial of n
+function factorialOf(n) {
+    if (n == 1) return 1
+    else return n * factorialOf(n - 1)
+}
+function latticePaths(gridSize) {
+    let numberOfWays = factorialOf(2 * gridSize) / (factorialOf(gridSize) * factorialOf(gridSize))
+    console.log(numberOfWays)
+    return numberOfWays;
+}
+
+latticePaths(20);
