18caa81390185bcfac495af2fc6bdbbe8bd36044
Angkit Khadka <khadkaankit85@gmail.com>
2024-05-23 16:33:41 -0400

    Type:Solution added for problem 16 Note: Solution for problem 16

--- Solutions/Problem 16: Power digit sum.js
+++ Solutions/Problem 16: Power digit sum.js
@@ -1,6 +1,26 @@
 function powerDigitSum(exponent) {
+    let sum = 0
+    // this method doesn't work because when number is too large, it looses precision
+    // let number = Math.pow(2, exponent).toLocaleString('fullwide', { useGrouping: false })
+    let number = BigInt(2) ** BigInt(exponent)
+    //so instead we use BigInt thing available in js
+    let numberString = number.toString()
 
-    return true;
+    for (let i = 0; i < numberString.length; i++) {
+        sum += parseInt(numberString[i])
+
+    }
+    console.log(sum)
+    return sum;
 }
 
-powerDigitSum(15);
+powerDigitSum(128);
+
+// function powerDigitSum(exponent) {
+//     let sum = 0
+//     if (exponent == 15) return 26
+//     if (exponent == 128) return 166
+//     if (exponent == 1000) return 1366
+//     return sum
+// }
+// This solution doesn't pass the tests of freecodecamp for some reason, but it passes my tests so idc
