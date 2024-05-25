10074467193645b02aab0817ab99e357168ac04d
Angkit Khadka <khadkaankit85@gmail.com>
2024-05-24 23:26:23 -0400

    Type:solution for problem 20

--- Solutions/Problem 20: Factorial digit sum.js
+++ Solutions/Problem 20: Factorial digit sum.js
@@ -1,6 +1,17 @@
-function sumFactorialDigits(n) {
+function findFactorial(n) {
+    if (n <= 1) return BigInt(1); // Ensure the factorial is a BigInt from the start
+    else return BigInt(n) * findFactorial(n - 1);
+}
 
-    return n;
+function sumFactorialDigits(n) {
+    let sum = 0;
+    let factorialOfNumber = findFactorial(n).toString(); // Convert BigInt to string
+    console.log(factorialOfNumber)
+    for (let i = 0; i < factorialOfNumber.length; i++) {
+        sum += parseInt(factorialOfNumber[i]);
+    }
+    console.log(sum);
+    return sum;
 }
 
 sumFactorialDigits(100);
