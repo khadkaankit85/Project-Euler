c1dd28aa04c0adbe9e704066cc5841d662869d16
Angkit Khadka <khadkaankit85@gmail.com>
2024-05-29 23:03:04 -0400

    code added but doesn't work dunno y

--- Solutions/Problem 19: Counting Sundays.js
+++ Solutions/Problem 19: Counting Sundays.js
@@ -1,23 +1,37 @@
+function countingSundays(startYear, endYear) {
+    let sundayCount = 0;
 
-function countingSundays(firstYear, lastYear) {
-    let sundayCount = 0
-    let date = []
-    for (let i = firstYear; i <= lastYear; i++) {
-        for (let j = 1; j <= 12; j++) {
-            let firstDayOfTheMonth = new Date(`${i}-${j}-02`)
-            if (firstDayOfTheMonth.getDay() == 1) {
-                sundayCount += 1
+    for (let year = startYear; year <= endYear; year++) {
+        for (let month = 1; month <= 12; month++) {
+            const firstDayOfMonth = new Date(year, month, 1);
+            if (firstDayOfMonth.getDay() === 0) { // Sunday
+                sundayCount++;
             }
-            if (i == lastYear) {
-                console.log("last year is ", new Date(`${i}-${j}-01`))
-            }
-            date.push(firstDayOfTheMonth)
         }
-
     }
-    console.log(date[date.length - 1])
-    return (sundayCount)
 
+    return sundayCount;
 }
-countingSundays(1995, 2000);
-//  yahh!!! so happy 
+
+// Test cases
+console.log(countingSundays(1943, 1946)); // should return 6
+console.log(countingSundays(1995, 2000)); // should return 10
+console.log(countingSundays(1901, 2000)); // should return 171
+
+
+// function countingSundays(firstYear, lastYear) {
+//     let sundayCount = 0;
+//     for (let i = firstYear; i <= lastYear; i++) {
+//         for (let j = 1; j <= 12; j++) {
+//             let firstDayOfTheMonth = new Date(i, j, 1);
+//             console.log(firstDayOfTheMonth)
+//             if (firstDayOfTheMonth.getDay() === 0) {
+//                 sundayCount++;
+//             }
+//         }
+//     }
+//     console.log(sundayCount);
+//     return sundayCount;
+// }
+
+// countingSundays(1995, 2000);
--- Solutions/tempCodeRunnerFile.js
+++ Solutions/tempCodeRunnerFile.js
@@ -1,19 +1,20 @@
+function countingSundays(startYear, endYear) {
+    let sundayCount = 0;
 
-function countingSundays(firstYear, lastYear) {
-    let sundayCount = 0
-    for (let i = firstYear; i <= lastYear; i++) {
-        for (let j = 1; j <= 12; j++) {
-            let firstDayOfTheMonth = new Date(`${i}-${j}-01`)
-            if (firstDayOfTheMonth.getDay() == 0) {
-                sundayCount += 1
-                console.log(firstDayOfTheMonth, " was sunday")
+    for (let year = startYear; year <= endYear; year++) {
+        for (let month = 1; month <= 12; month++) {
+            const firstDayOfMonth = new Date(year, month, 1);
+            console.log(firstDayOfMonth)
+            if (firstDayOfMonth.getDay() === 0) { // Sunday
+                sundayCount++;
             }
         }
-
     }
-    console.log(sundayCount)
-    return (sundayCount)
 
+    return sundayCount;
 }
-countingSundays(1901, 2000);
-//  yahh!!! so happy 
+
+// Test cases
+console.log(countingSundays(1943, 1946)); // should return 6
+console.log(countingSundays(1995, 2000)); // should return 10
+console.log(countingSundays(1901, 2000)); // should return 171
