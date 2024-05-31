e9a1df11ad86761d8058374f2d49f5887cd43c6a
Angkit Khadka <khadkaankit85@gmail.com>
2024-05-30 23:54:04 -0400

    Q25 problem

--- Solutions/Problem 25: 1000-digit Fibonacci number.js
+++ Solutions/Problem 25: 1000-digit Fibonacci number.js
@@ -1,6 +1,43 @@
-function digitFibonacci(n) {
+// function findFibonacci(n) {
+//     if (n <= 2) {
+//         return 1
+//     }
+//     if (n == 3) {
+//         return 2
+//     }
+//     else {
+//         return findFibonacci(n - 1) + findFibonacci(n - 2)
+//     }
 
-    return n;
+// }
+// this method of finding fibonacci has an exponential time complexity 
+// O(2 to the power n) so we gotta find another way
+function findFibonacci(index) {
+    if (index <= 2) return 1
+    let a = 1
+    let b = 1
+    let fibo = 1
+    for (let i = 3; i <= index; i++) {
+        fibo = a + b
+        a = b
+        b = fibo
+
+    }
+    return fibo
 }
 
+
+function digitFibonacci(n) {
+    let i = 1
+    while (true) {
+        let fibo = findFibonacci(i)
+        if (fibo.toString().length == n) {
+            console.log(i)
+            break
+        }
+        i += 1
+    }
+    return i
+
+}
 digitFibonacci(20);
