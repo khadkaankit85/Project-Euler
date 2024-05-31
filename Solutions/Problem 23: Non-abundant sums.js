88301cfa9634461ca227a30c66b78992aa6a16cc
Angkit Khadka <khadkaankit85@gmail.com>
2024-05-30 22:57:40 -0400

    Q 23 Solution

--- Solutions/Problem 23: Non-abundant sums.js
+++ Solutions/Problem 23: Non-abundant sums.js
@@ -1,6 +1,52 @@
-function sumOfNonAbundantNumbers(n) {
+function findSumOfProperDivisors(n) {
+    let sum = 0
+    for (let i = 1; i <= n / 2; i++) {
+        if (Number.isInteger(n / i)) {
+            sum += i
+        }
+    }
+    return sum
 
-    return n;
+}
+function isAbundant(n) {
+    if (findSumOfProperDivisors(n) <= n) return false
+    return true
+
 }
 
-sumOfNonAbundantNumbers(28123);
+function sumOfNonAbundantNumbers(limit) {
+    //  to find abundant numbers
+    let AbundantNumberList = []
+    for (let i = 1; i <= limit; i++) {
+        if (isAbundant(i)) AbundantNumberList.push(i)
+    }
+
+    //to check if a number can be written as the sum of two abundant numbers
+
+    let canBeWrittenAsTwoSum = new Array(limit + 1).fill(false)
+    for (let i = 0; i <= limit; i++) {
+
+        for (let j = i; j <= AbundantNumberList.length; j++) {
+            const sum = AbundantNumberList[i] + AbundantNumberList[j]
+            if (sum <= limit) {
+                canBeWrittenAsTwoSum[sum] = true
+            }
+            else {
+                break
+            }
+        }
+    }
+
+    let sumOfNonAbundantSums = 0;
+    for (let i = 1; i <= limit; i++) {
+        if (!canBeWrittenAsTwoSum[i]) {
+            sumOfNonAbundantSums += i;
+        }
+    }
+
+    return sumOfNonAbundantSums;
+
+
+}
+
+console.log(sumOfNonAbundantNumbers(28123));
