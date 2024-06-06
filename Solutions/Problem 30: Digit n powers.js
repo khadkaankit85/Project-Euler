06a897393e7066ccba676e8804156f2ecd18c90c
Angkit Khadka <khadkaankit85@gmail.com>
2024-06-05 22:43:31 -0400

    Q30 solved

--- Solutions/Problem 30: Digit n powers.js
+++ Solutions/Problem 30: Digit n powers.js
@@ -1,6 +1,20 @@
-function digitnPowers(n) {
+function canBeWrittenAsThePowerOfn(number, n) {
+    let sumOfnPowerOfEach = number.toString().split("")
+        .reduce((p, c) => {
+            return p + c ** n
+        }, 0)
+    if (number == sumOfnPowerOfEach) return true
+    else return false
 
-    return n;
 }
 
-digitnPowers(5);
+function digitnPowers(n) {
+    let sum = 0
+    for (let i = 2; i <= 200000; i++) {
+        if (canBeWrittenAsThePowerOfn(i, n)) {
+            sum += i
+        }
+    }
+    return sum;
+}
+console.log(digitnPowers(5));
