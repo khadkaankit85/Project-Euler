c068b981947711d7b47dc5e5eebac236ebb16df2
Angkit Khadka <khadkaankit85@gmail.com>
2024-09-01 21:31:49 -0400

    solution 39

--- Solutions/Problem 39: Integer right triangles.js
+++ Solutions/Problem 39: Integer right triangles.js
@@ -1,6 +1,30 @@
 function intRightTriangles(n) {
+    const perimeterCount = {};
 
-    return n;
+    for (let a = 1; a <= n / 2; a++) {
+        for (let b = a; b <= (n - a) / 2; b++) {
+            const c = Math.sqrt(a * a + b * b);
+            if (Number.isInteger(c)) {
+                const p = a + b + c;
+                if (p <= n) {
+                    perimeterCount[p] = (perimeterCount[p] || 0) + 1;
+                }
+            }
+        }
+    }
+
+    let maxCount = 0;
+    let maxPerimeter = 0;
+
+    for (const p in perimeterCount) {
+        if (perimeterCount[p] > maxCount) {
+            maxCount = perimeterCount[p];
+            maxPerimeter = parseInt(p, 10);
+        }
+    }
+
+    return maxPerimeter;
 }
 
-intRightTriangles(500);
+console.log(intRightTriangles(500));
+console.log(intRightTriangles(800));  
